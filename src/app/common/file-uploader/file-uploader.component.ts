d9354a5fc3d574ee71ba04ae18591a1d8476f019
AnirudhNJ <anellippillijos@deakin.edu.au>
2024-09-20 20:47:08 +1000

    refactor: add file-uploader css_fix

--- src/app/common/file-uploader/file-uploader.component.ts
+++ src/app/common/file-uploader/file-uploader.component.ts
@@ -13,7 +13,9 @@ export class FileUploaderComponent implements OnInit {
   @Input() url: string = '';
   @Input() method: string = 'POST';
   @Input() payload: any = {};
-  @Input() onBeforeUpload?: () => void;
+  // @Input() onBeforeUpload?: () => void;
+  // eslint-disable-next-line @angular-eslint/no-output-on-prefix
+  @Output() onBeforeUpload? = new EventEmitter<any>();
   // @Input() onSuccess?: (response: any) => void;
   // eslint-disable-next-line @angular-eslint/no-output-on-prefix
   @Output() onSuccess = new EventEmitter<any>();
@@ -56,9 +58,11 @@ export class FileUploaderComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    console.log('t5gkt', this.uploadZones);
     if (!this.files || typeof this.files !== 'object') {
       this.files = {};
     }
+    console.log('t5gkt', this.files);
     this.createUploadZones(this.files);
     if (!this.onClickFailureCancel) {
       this.onClickFailureCancel = this.resetUploader.bind(this);
@@ -70,8 +74,32 @@ export class FileUploaderComponent implements OnInit {
     const files = Array.from(event.dataTransfer!.files);
     this.handleFiles(files);
   }
-  handleFiles(_files: File[]) {
-    throw new Error('Method not implemented.');
+  // handleFiles(_files: File[]) {
+  //   throw new Error('Method not implemented.');
+  // }
+  handleFiles(files: File[]): void {
+    files.forEach((file) => {
+      this.uploadZones.forEach((zone) => {
+        const extensions = zone.accept.split(',').map((ext) => ext.trim());
+        if (extensions.some((ext) => file.name.endsWith(ext))) {
+          zone.model = [file];
+          this.refreshShownUploadZones();
+        } else {
+          zone.rejects = [file];
+          this.checkForError(zone);
+        }
+      });
+    });
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDragLeave(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
   }
 
   createUploadZones(files: any): void {
@@ -106,7 +134,7 @@ export class FileUploaderComponent implements OnInit {
     this.isUploading = false;
     this.showUploader = !this.asButton;
 
-    for (let upload of this.uploadZones) {
+    for (const upload of this.uploadZones) {
       this.clearEnqueuedUpload(upload);
     }
   }
@@ -160,14 +188,14 @@ export class FileUploaderComponent implements OnInit {
       return;
     }
 
-    this.onBeforeUpload?.();
+    this.onBeforeUpload?.emit();
 
     const formData = new FormData();
-    for (let zone of this.uploadZones) {
+    for (const zone of this.uploadZones) {
       formData.append(zone.name, zone.model[0]);
     }
 
-    for (let key in this.payload) {
+    for (const key in this.payload) {
       let value = this.payload[key];
       if (typeof value === 'object') {
         value = JSON.stringify(value);
@@ -209,7 +237,7 @@ export class FileUploaderComponent implements OnInit {
           this.uploadingInfo.success = false;
           this.uploadingInfo.error = error.error || 'Unknown error';
           this.onFailure.emit(error.message);
-        }
+        },
       });
   }
 
